ab8f612de9295dc5b911a1255e1f4bcb2a19578d
TSMLocalize <50493647+TSMLocalize@users.noreply.github.com>
2020-04-27 23:51:16 +0100

    Timers Log now fully function (Needs Enemy implementation)

--- Text Game/Assets/Scripts/Timers_Entry.cs
+++ Text Game/Assets/Scripts/Timers_Entry.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timers_Entry : MonoBehaviour
 {
@@ -10,20 +11,30 @@ public class Timers_Entry : MonoBehaviour
     public float currentValue;
     public Image[] TimerImageArray;
     public bool castMode;
+    public bool readyMode;
+    public bool waitMode;
+    public TextMeshProUGUI NumberText;
+    public TextMeshProUGUI MainText;    
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         TimerImageArray = this.GetComponentsInChildren<Image>();
         TimerImageArray[1].overrideSprite = TimersEntryPlayer.PlayerPortrait;
+
+        if (readyMode)
+        {
+            TimerImageArray[2].color = new Color32(0, 150, 0, 255);
+        }
         
         if (castMode)
-        {
+        {           
             TimerImageArray[2].color = Color.magenta;
         }
-        else
-        {
+        
+        if (waitMode)
+        {            
             TimerImageArray[2].color = Color.blue;
-        }
+        }        
     }
 }
--- Text Game/Assets/Scripts/Timers_Log.cs
+++ Text Game/Assets/Scripts/Timers_Log.cs
@@ -19,7 +19,7 @@ public class Timers_Log : MonoBehaviour
     public List<GameObject> countOrdered;
     public Image[] TimerImageArray;
     public TextMeshProUGUI[] instantiatedTimersTextArray;
-    public Timers_Entry pfTimer_Entry;
+    public Timers_Entry pfTimer_Entry;    
     public bool battleStart = true;
 
     public List<Vector3> ListPositions;
@@ -39,28 +39,44 @@ public class Timers_Log : MonoBehaviour
             {
                 pfTimer_Entry.TimersEntryPlayer = BM.PlayersInBattle[i];
                 BM.PlayersInBattle[i].playerTimersEntry = Instantiate(pfTimer_Entry.gameObject, TimersPanel.transform);
-                TimerList.Add(BM.PlayersInBattle[i].playerTimersEntry);
+                TimerList.Add(BM.PlayersInBattle[i].playerTimersEntry);                
             }
+            
             battleStart = false;
         }
 
         updatePositions();
     }
 
+    //This updates the position and color of each TimersEntry every frame
     public void updatePositions()
-    {
+    {        
         for (int i = 0; i < BM.PlayersInBattle.Count; i++)
         {
-            if (BM.PlayersInBattle[i].isCastingSpell)
+            BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().readyMode = false;
+            BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().waitMode = false;
+            BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().castMode = false;
+
+            if (BM.ActivePlayers.Contains(BM.PlayersInBattle[i]))
+            {
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().NumberText.text = "--";
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().MainText.text = "READY!";
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().readyMode = true;
+            } 
+            else if (BM.PlayersInBattle[i].isCastingSpell)
             {
                 BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().currentValue =
                     (BM.PlayersInBattle[i].castSpeedTotal / BM.PlayersInBattle[i].castSpeed);
-                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().castMode = true;
-
-            } else
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().castMode = true;                
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().NumberText.text = "@" + Mathf.RoundToInt(BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().currentValue);
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().MainText.text = "Spell Ready:";
+            }
+            else
             {                
                 BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().currentValue = ((100 - BM.PlayersInBattle[i].speedTotal) / BM.PlayersInBattle[i].speed);
-                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().castMode = false;
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().waitMode = true;                
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().NumberText.text = "@" + Mathf.RoundToInt(BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().currentValue);
+                BM.PlayersInBattle[i].playerTimersEntry.GetComponent<Timers_Entry>().MainText.text = "Next Turn:";
             }
         }      
 
@@ -73,68 +89,4 @@ public class Timers_Log : MonoBehaviour
 
         Canvas.ForceUpdateCanvases();
     }
-
-    /*
-    public void updateTimersLog()
-    {
-        instantiatedTimersOptions = instantiatedTimersOptions.OrderByDescending(instantiatedTimersOption => Mathf.CeilToInt(instantiatedTimersOption.timeTilTurn)).ToList();
-
-        for (int i = 0; i < instantiatedTimersOptions.Count; i++)
-        {
-            Destroy(instantiatedTimersOptions[i].timersEntry);
-            instantiatedTimersOptions[i].timersEntry = Instantiate(pfTimersOption, TimersOptionPanel.transform);            
-            instantiatedTimersImageArray = instantiatedTimersOptions[i].timersEntry.GetComponentsInChildren<Image>();
-            instantiatedTimersTextArray = instantiatedTimersOptions[i].timersEntry.GetComponentsInChildren<TextMeshProUGUI>();
-
-            if (instantiatedTimersOptions[i].player != null && instantiatedTimersOptions[i].player.isCastingSpell)
-            {
-                instantiatedTimersOptions[i].timeTilTurn = Mathf.CeilToInt(instantiatedTimersOptions[i].player.castSpeedTotal / instantiatedTimersOptions[i].player.castSpeed);
-
-                instantiatedTimersImageArray[1].overrideSprite = instantiatedTimersOptions[i].player.PlayerPortrait;
-
-                instantiatedTimersTextArray[0].text =
-                    instantiatedTimersOptions[i].player.name + "\n" +
-                    instantiatedTimersOptions[i].player.activeSpell.name + " @" + instantiatedTimersOptions[i].timeTilTurn;
-
-                instantiatedTimersImageArray[2].transform.localScale =
-                    new Vector3(Mathf.Clamp((instantiatedTimersOptions[i].player.castSpeedTotal / instantiatedTimersOptions[i].player.activeSpell.castTime), 0, 1),
-                        instantiatedTimersImageArray[2].transform.localScale.y, instantiatedTimersImageArray[2].transform.localScale.z);
-            }
-            else if (instantiatedTimersOptions[i].enemy != null && instantiatedTimersOptions[i].enemy.isCastingSpell)
-            {
-                instantiatedTimersOptions[i].timeTilTurn = Mathf.CeilToInt(instantiatedTimersOptions[i].enemy.castSpeedTotal / instantiatedTimersOptions[i].enemy.castSpeed);
-
-                instantiatedTimersImageArray[1].overrideSprite = instantiatedTimersOptions[i].enemy.EnemyPortrait;
-
-                instantiatedTimersTextArray[0].text =
-                    instantiatedTimersOptions[i].enemy.EnemyName + "\n" +
-                    instantiatedTimersOptions[i].enemy.activeSpell.name + " @" + instantiatedTimersOptions[i].timeTilTurn;
-
-                instantiatedTimersImageArray[2].transform.localScale =
-                    new Vector3(Mathf.Clamp((instantiatedTimersOptions[i].enemy.castSpeedTotal / instantiatedTimersOptions[i].enemy.activeSpell.castTime), 0, 1),
-                        instantiatedTimersImageArray[2].transform.localScale.y, instantiatedTimersImageArray[2].transform.localScale.z);
-            } 
-            else if (instantiatedTimersOptions[i].player != null && instantiatedTimersOptions[i].player.isCastingSpell == false)
-            {
-                instantiatedTimersOptions[i].timeTilTurn = Mathf.CeilToInt((100 - instantiatedTimersOptions[i].player.speedTotal) / instantiatedTimersOptions[i].player.speed);
-
-                instantiatedTimersImageArray[1].overrideSprite = instantiatedTimersOptions[i].player.PlayerPortrait;
-
-                instantiatedTimersTextArray[0].text =
-                    instantiatedTimersOptions[i].player.name + "\n" + " @" + instantiatedTimersOptions[i].timeTilTurn;
-
-                instantiatedTimersImageArray[2].color = Color.blue;
-
-                instantiatedTimersImageArray[2].transform.localScale =
-                    new Vector3(Mathf.Clamp((((100 - instantiatedTimersOptions[i].player.speedTotal) / instantiatedTimersOptions[i].player.speed) / 10), 0, 1),
-                        instantiatedTimersImageArray[2].transform.localScale.y, instantiatedTimersImageArray[2].transform.localScale.z);
-            }
-            else
-            {
-                Destroy(instantiatedTimersOptions[i].timersEntry);
-                instantiatedTimersOptions.Remove(instantiatedTimersOptions[i]);
-            }
-        }
-    }
-    */
 }
