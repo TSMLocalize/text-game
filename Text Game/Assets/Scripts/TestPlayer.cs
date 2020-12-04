5067db8f422cc41d2eaee657aa1ef0b3d6f868d2
TSMLocalize <50493647+TSMLocalize@users.noreply.github.com>
2020-12-04 00:13:57 +0000

    implemented grid into the game

---
+++ Text Game/Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    //Grid Variables
+    public float x;
+    public float y;
+    public List<GameObject> path;
+    public GameObject currentTile;
+}
--- Text Game/Assets/Scripts/Tile.cs
+++ Text Game/Assets/Scripts/Tile.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float x;
+    public float y;
 
-    // Update is called once per frame
+    public GameObject parent;
+
+    public List<GameObject> Neighbours;
+
+    public float gCost;
+    public float hCost;
+
+    public bool tested;
+
+    public GridMap map;
+
     void Update()
     {
-        
+
+    }
+
+    private void Start()
+    {
+        map = FindObjectOfType<GridMap>();
+    }
+
+    void OnMouseDown()
+    {
+        if (map.isMoving == false)
+        {
+            map.selectedTile = this.gameObject;
+        }
+    }
+
+    public float fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
     }
 }
