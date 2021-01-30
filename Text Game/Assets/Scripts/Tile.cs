9d3851429f79e9e2006a9fec4b60fc8b3e439052
Timothy <ryalin1286@gmail.com>
2021-01-30 12:03:33 +0000

    Revert "Removed Grid Nonsense"
    This reverts commit dcae5abbfdbd5aa95da56337f5eb3c1fd6d530b3.

---
+++ Text Game/Assets/Scripts/Tile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public float x;
+    public float y;
+
+    public GameObject parent;
+
+    public List<GameObject> Neighbours;
+
+    public float gCost;
+    public float hCost;
+
+    public bool tested;
+
+    public GridMap map;
+
+    void Update()
+    {
+
+    }
+
+    private void Start()
+    {
+        map = FindObjectOfType<GridMap>();
+    }
+
+    void OnMouseDown()
+    {
+        for (int i = 0; i < map.tiles.Count; i++)
+        {
+            map.tiles[i].GetComponentInChildren<SpriteRenderer>().color = Color.white;
+        }
+
+
+        if (map.isMoving == false)
+        {
+            map.selectedTile = this.gameObject;
+        }
+
+        for (int i = 0; i < this.Neighbours.Count; i++)
+        {
+            this.Neighbours[i].GetComponentInChildren<SpriteRenderer>().color = Color.red;
+        }
+
+    }
+
+    public float fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+}
