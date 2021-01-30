e9897778b6ef131e559377cf5f87552ff014f365
Timothy <ryalin1286@gmail.com>
2021-01-30 12:03:46 +0000

    Revert "implemented grid into the game"
    This reverts commit 5067db8f422cc41d2eaee657aa1ef0b3d6f868d2.

--- Text Game/Assets/Scripts/Tile.cs
+++ Text Game/Assets/Scripts/Tile.cs
@@ -4,43 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public float x;
-    public float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    public GameObject parent;
-
-    public List<GameObject> Neighbours;
-
-    public float gCost;
-    public float hCost;
-
-    public bool tested;
-
-    public GridMap map;
-
+    // Update is called once per frame
     void Update()
     {
-
-    }
-
-    private void Start()
-    {
-        map = FindObjectOfType<GridMap>();
-    }
-
-    void OnMouseDown()
-    {
-        if (map.isMoving == false)
-        {
-            map.selectedTile = this.gameObject;
-        }
-    }
-
-    public float fCost
-    {
-        get
-        {
-            return gCost + hCost;
-        }
+        
     }
 }
